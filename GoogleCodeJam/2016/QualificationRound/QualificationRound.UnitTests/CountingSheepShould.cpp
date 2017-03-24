804f484b1f55e2a64b559d867352c00390ad90f4
Bastien <bastien.teinturier@outlook.com>
2017-03-24 22:28:29 +0100

    Create unit test project

---
+++ GoogleCodeJam/2016/QualificationRound/QualificationRound.UnitTests/CountingSheepShould.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace QualificationRoundUnitTests
+{		
+	TEST_CLASS(CountingSheepShould)
+	{
+	public:
+		
+		TEST_METHOD(ReturnInsomniaForZero)
+		{
+			Assert::Fail();
+		}
+
+	};
+}
