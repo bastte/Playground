7a4d955b8edf7f79200b1df03f6f2506a9804ba2
Bastien Teinturier <bastte@microsoft.com>
2017-05-10 18:05:55 +0200

    Implement the RPN calculator

--- Haskell/src/RPNCalculator.hs
+++ Haskell/src/RPNCalculator.hs
@@ -1,4 +1,19 @@
 module RPNCalculator (rpn) where
 
-rpn :: (Num a) => String -> a
-rpn = undefined
+rpn :: String -> Int
+rpn = head . eval . tokenize . words
+    where 
+        tokenize = map parseToken
+        eval = foldl (flip ($)) []
+
+-- Performs an action on the evaluation stack depending on the current token
+type TokenAction = [Int] -> [Int]
+
+parseToken :: String -> TokenAction
+parseToken token = case token of
+    "+" -> binaryStackOperation (+)
+    "-" -> binaryStackOperation (-)
+    "*" -> binaryStackOperation (*)
+    "/" -> binaryStackOperation div
+    _   -> (:) (read token)
+    where binaryStackOperation f (x1:x2:xs) = f x1 x2 : xs
--- Haskell/test/RPNCalculatorSpec.hs
+++ Haskell/test/RPNCalculatorSpec.hs
@@ -6,9 +6,9 @@ import RPNCalculator
 spec :: Spec
 spec = 
     describe "RPN Calculator" $ do
-        context "with 1 2 + 3 *" $
-            it "shouldbe 9" $
-                rpn "1 2 + 3 *" `shouldBe` 9
+        context "with 1 2 + 3 * 17 -" $
+            it "shouldbe 8" $
+                rpn "1 2 + 3 * 17 -" `shouldBe` 8
 
 main :: IO ()
 main = hspec spec
